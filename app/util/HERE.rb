2639879a4c4b696fcd5f70c11b7068600c7b036e
NicholasS13 <37513914+NicholasS13@users.noreply.github.com>
2020-08-12 21:59:32 -0400

    HERE API for travel time calculations (#99)
    * added HERE API for time calculations
    
    * Update app/util/HERE.rb
    
    Co-authored-by: Drew Hite <sagehen03@users.noreply.github.com>
    
    Co-authored-by: Drew Hite <sagehen03@users.noreply.github.com>

---
+++ app/util/HERE.rb
@@ -0,0 +1,38 @@
+require 'net/http'
+require 'json'
+class HEREAPI
+
+    class PointLocation
+        attr_reader :latitude, :longitude
+        def initialize(latitude, longitude)
+            @latitude = latitude
+            @longitude = longitude
+        end
+    end
+
+    module TravelMode
+        BIKE = 'bicycle'
+        CAR = 'car'
+        PUBLIC_TRANSPORT = 'publicTransport'
+    end
+
+    def self.get_time(origin, destination, travel_mode=TravelMode::CAR, rounding=2)
+        here_url = "https://route.ls.hereapi.com/routing/7.2/calculateroute.json?apiKey=%<here_key>s&waypoint0=geo!%<origin_lat>s"\
+                    ",%<origin_long>s&waypoint1=geo!%<dest_lat>s,%<dest_long>s&representation=overview&mode=fastest;%<travel_mode>s;traffic:#{travel_mode == TravelMode::CAR ? 'enabled' : 'disabled'}"
+                    .% origin_lat: origin.latitude, origin_long: origin.longitude, dest_lat: destination.latitude,
+                    dest_long: destination.longitude, travel_mode: travel_mode, here_key: ENV["HERE_API_KEY"]
+        uri = URI(here_url)
+        res = Net::HTTP.get_response(uri)
+        unless res.is_a? Net::HTTPSuccess
+            puts res.body
+            return "Error: #{res.message}"
+        end
+        begin
+            res_data = JSON.parse(res.body)
+            time = res_data['response']['route'][0]['summary']['travelTime'] / 60.0
+            time.round(rounding)
+        rescue
+            return "Error: could not parse JSON response"
+        end
+    end
+end

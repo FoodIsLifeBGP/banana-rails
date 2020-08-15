21d64f942f1627928b3952f8cbac22bb32cdb483
Drew Hite <drew@drewhite.org>
2020-08-15 00:18:36 -0400

    stop including all donations as part of the donor login response

--- app/serializers/donor_serializer.rb
+++ app/serializers/donor_serializer.rb
@@ -9,8 +9,7 @@ class DonorSerializer < ActiveModel::Serializer
     :address_state,
     :address_zip,
     :account_status,
-    :pickup_instructions,
-    :donations
+    :pickup_instructions
     # :business_license,        # commented out for pre-alpha
     # :business_phone_number,   # commented out for pre-alpha
     # :business_doc_id,         # commented out for pre-alpha

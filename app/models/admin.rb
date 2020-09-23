020b24fee6dd794bb75afb9f4f57dffbb671dc8a
trdtnguyen <trauconnguyen@gmail.com>
2020-09-23 16:30:12 -0500

    (1) Add APIs for admin settings. (2) Add new admins_controller. (3) Modify admin model to validate data.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,8 @@
 class Admin < ApplicationRecord
 	has_secure_password	
+
+    validates :email, presence: true
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+    #validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).{8,40}\z/}
 end

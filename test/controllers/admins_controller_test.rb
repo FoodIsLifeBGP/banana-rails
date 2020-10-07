742dd4ce089c1213728554a3d8ca2bb20f3dffab
trdtnguyen <trauconnguyen@gmail.com>
2020-10-07 16:59:23 -0500

    add tester for admin controller

--- test/controllers/admins_controller_test.rb
+++ test/controllers/admins_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class AdminsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "we return 409 status code in the event the admin email is already present in the db" do
+    post admins_create_url, params: {admin: { email: "admin1@admin.com", password: "does not matter"}}
+    assert_response :conflict
+  end
+
+  test "we successfully register a new admin" do
+    post admins_create_url, params: {admin: { email: "newadmin@admin.com",
+                                              password: "password1!",
+                                              first_name: "New",
+                                              last_name: "Admin",
+                                              user_type: "admin",
+                                              user_status: AdminStatus::ACTIVE
+    }}
+    assert_response :success
+    just_added = Admin.find_by_email("newadmin@admin.com")
+    assert_not_nil just_added
+  end
+
+  test "update name of the admin" do
+      patch '/admins/1/update', params: {admin: {first_name: "NewAdminName1"}},
+          headers: auth_header({admin_id: 1})
+      assert_response 200
+  end
 end
